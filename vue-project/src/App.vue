0814e61a7de1bde62d00b32f26a788c45a3b4329
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-04 14:42:33 +0300

    Task 8

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,18 +2,14 @@
 export default{
 data(){
   return{
-    arr: ['x', 'y', 'z'],
-    arr1: [1, 2, 3],
+    obj: {x: 1, y: 2, z: 3},
   }
 }
 }
 </script>
 
 <template>
-<div class="one">{{ arr[0] }}</div>
-<div class="one">{{ arr[1] }}</div>
-<div class="one">{{ arr[2] }}</div>
-<div class="one">{{ arr1[0] + arr1[1] + arr1[2] }}</div>
+<div class="one">{{ obj.x + obj.y + obj.z }}</div>
 </template>
 
 <style scoped>
