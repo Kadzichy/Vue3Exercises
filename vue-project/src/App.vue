2ba93251aa36cd49dc59b276183425f8cf31c540
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-04 15:34:33 +0300

    Task 17

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,26 +2,17 @@
 export default{
 data(){
   return{
-    num1: 1,
-    num2: 2,
-    text: 'Sum'
+    visible1: true,
+    visible2: false,
   }
 },
-methods: {
-  show: function(){
-    let text = this.cape(this.text)
-    alert(this.num1 + this.num2);
-  },
-  cape: function(str){
-    return str[0].toUpperCase() + str.slice(1);
-  }
-}
 }
 </script>
 
 <template>
-<a class="inline-link-3" href="https://www.youtube.com/" onclick="event.preventDefault()">Link</a>
-<button class="button" @click.once="show">click</button>
+<div class="one" v-if="visible1">Oleg</div>
+<div class="one" v-if="!visible2">Tinkoff</div>
+
 </template>
 
 <style scoped>
