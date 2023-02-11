3138ce6f6b0bab1938db87a849c862a2c6c65680
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 11:06:29 +0300

    Task 23

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -11,13 +11,13 @@ methods: {
 </script>
 
 <template>
-<p class="one" v-if="day === 'Понедельник'">Понедельник</p>
-<p class="one" v-if="day === 'Вторник'">Вторник</p>
-<p class="one" v-if="day === 'Среда'">Среда</p>
-<p class="one" v-if="day === 'Четверг'">Четверг</p>
-<p class="one" v-if="day === 'Пятница'">Пятница</p>
-<p class="one" v-if="day === 'Суббота'">Суббота</p>
-<p class="one" v-if="day === 'Воскресенье'">Воскресенье</p>
+<div class="one" v-if="day === 'Понедельник'">Понедельник</div>
+<div class="one" v-if="day === 'Вторник'">Вторник</div>
+<div class="one" v-if="day === 'Среда'">Среда</div>
+<div class="one" v-if="day === 'Четверг'">Четверг</div>
+<div class="one" v-if="day === 'Пятница'">Пятница</div>
+<div class="one" v-if="day === 'Суббота'">Суббота</div>
+<div class="one" v-if="day === 'Воскресенье'">Воскресенье</div>
 </template>
 
 <style scoped>
