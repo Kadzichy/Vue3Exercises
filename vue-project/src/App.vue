8dae0c1b7e5553edafa07fa1c9d849b7029a5ce2
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 10:59:18 +0300

    Task 22

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,34 +2,17 @@
 export default{
 data(){
   return{
-    visible: true,
-    visible2: true,
-    visible3: true,
+    isAdmin: false,
   }
 },
 methods: {
-  toggle: function(){
-    this.visible = !this.visible;
-  },
-  toggle2: function () {
-    this.visible2 = !this.visible2;
-  },
-  toggle3: function () {
-    this.visible3 = !this.visible3;
-  }
 }
 }
 </script>
 
 <template>
-<button class="button" @click="toggle">What</button>
-<div class="one">{{visible ? 'Oleg' : 'tinkoff'}}</div>
-<button class="button" @click="toggle2">you</button>
-<div class="one">{{visible2 ? 'Yes' : 'No'}}</div>
-<button class="button" @click="toggle3">say</button>
-<div class="one" >{{visible3 ? 'Oleg' : 'Gazmanov'}}</div>
-
-
+<div class="one" v-if="isAdmin">Oleg tinkoff</div>
+<div class="one" v-else>Oleg Gazmanov</div>
 </template>
 
 <style scoped>
