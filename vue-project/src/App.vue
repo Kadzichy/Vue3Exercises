dce6f20c9bcc13db3b85fdf55611c0ab26042530
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 11:01:56 +0300

    Task 23

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,7 +2,7 @@
 export default{
 data(){
   return{
-    isAdmin: false,
+    day: 'Вторник',
   }
 },
 methods: {
@@ -11,8 +11,13 @@ methods: {
 </script>
 
 <template>
-<div class="one" v-if="isAdmin">Oleg tinkoff</div>
-<div class="one" v-else>Oleg Gazmanov</div>
+<p class="one" v-if="day === 'Понедельник'">Понедельник</p>
+<p class="one" v-if="day === 'Вторник'">Вторник</p>
+<p class="one" v-if="day === 'Среда'">Среда</p>
+<p class="one" v-if="day === 'Четверг'">Четверг</p>
+<p class="one" v-if="day === 'Пятница'">Пятница</p>
+<p class="one" v-if="day === 'Суббота'">Суббота</p>
+<p class="one" v-if="day === 'Воскресенье'">Воскресенье</p>
 </template>
 
 <style scoped>
