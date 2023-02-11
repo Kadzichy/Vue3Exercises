137588a8e8ff1fdccf00a278249f858845d0e4a0
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 10:43:55 +0300

    Task 19

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,16 +2,31 @@
 export default{
 data(){
   return{
-    hidden1: true,
-    hidden2: false,
+    visible: true,
+btnHide: true,
+btnVis: false,
   }
 },
+methods: {
+  hide: function(){
+    this.visible = false;
+    this.btnHide = false;
+    this.btnVis = true;
+  },
+  vis: function(){
+    this.visible = true;
+    this.btnHide = true;
+    this.btnVis = false;
+  }
+}
 }
 </script>
 
 <template>
 <div class="one" v-if="hidden1">Oleg</div>
 <div class="one" v-if="!hidden2">Tinkoff</div>
+<button class="button" @click="hide" v-if="btnHide">hide</button>
+<button class="button" @click="vis" v-if="btnVis">visible</button>
 
 </template>
 
