aeef1a1e706f2740c9782e050b0ad554e817413e
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 11:21:16 +0300

    Task 27

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,20 +2,18 @@
 export default{
 data(){
   return{
-    isAuth: true,
+    items: [1, 2, 3, 4, 5],
   }
 },
 methods: {
-toggle: function(){
-  this.isAuth = false;
-}
 }
 }
 </script>
 
 <template>
-  <button class="button" @click="toggle">Oleg</button>
-  <p class="one" v-show="isAuth">Oleg Tinkoff</p>
+  <div class="one" v-for="elem in items">{{ elem }}</div>
+  <div class="one" v-for="elem in items">{{ elem*elem }}</div>
+  <ul class="one" v-for="elem in items">{{ elem }}</ul>
 </template>
 
 <style scoped>
