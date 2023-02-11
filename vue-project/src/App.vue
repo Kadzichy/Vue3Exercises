0672ca1848595031e1028c1c0f95eca60f1c0e1a
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-11 11:04:40 +0300

    Task 24

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,7 +2,7 @@
 export default{
 data(){
   return{
-    day: 'Вторник',
+    Age: 15,
   }
 },
 methods: {
@@ -11,13 +11,9 @@ methods: {
 </script>
 
 <template>
-<p class="one" v-if="day === 'Понедельник'">Понедельник</p>
-<p class="one" v-if="day === 'Вторник'">Вторник</p>
-<p class="one" v-if="day === 'Среда'">Среда</p>
-<p class="one" v-if="day === 'Четверг'">Четверг</p>
-<p class="one" v-if="day === 'Пятница'">Пятница</p>
-<p class="one" v-if="day === 'Суббота'">Суббота</p>
-<p class="one" v-if="day === 'Воскресенье'">Воскресенье</p>
+<p class="one" v-if="Age < 18">Подросток</p>
+<p class="one" v-else-if="19 < Age && age < 25">Молодой человек</p>
+<p class="one" v-else="Age > 26">Мужчина</p>
 </template>
 
 <style scoped>
