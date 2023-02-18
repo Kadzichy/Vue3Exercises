f399c4789e70e13ea88012f4f8e65c58dde03409
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-18 15:51:38 +0300

    Task 44

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,21 +2,33 @@
 export default{
 data(){
   return{
-message: '',
-number: '',
+    res: 0,
+    sum: 0,
+    num: 0,
+    num1: 0, 
+    num2: 0,
   }
 },
 methods: {
+  calc: function () {
+    this.res = Math.sqrt(this.num);
+  },
+  summary:function(){
+    this.sum = this.num1 + this.num2;
+  }
 }
 }
 </script>
 
 <template>
-<input class="text-field__input" v-model="message">
-<p class="one" :style="{textTransform: 'uppercase'}">{{ message }}</p>
-<br>
-<input class="text-field__input" v-model="number">
-<p class="one">{{ number*number }}</p>
+<p class="one">{{ res }}</p>
+<input class="text-field__input" v-model="num">
+<button class="button" @click="calc">Sqrt</button>
+<p class="one">{{ sum }}</p>
+<input class="text-field__input" v-model=num1>
+<input class="text-field__input" v-model=num2>
+<button class="button" @click="summary">Sum</button>
+
 </template>
 
 <style scoped>
