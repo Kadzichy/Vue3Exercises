e053f29798de9e1cc22846298bdc536ab95e3718
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-18 15:42:24 +0300

    Task 39

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,22 +2,37 @@
 export default{
 data(){
   return{
-    obj: {
-      done: true,
-      selected: false,
+    visible: true,
+    styles: {
+      hidden: true,
     },
   }
 },
 methods: {
+setDone:function(){
+  this.styles.hidden = false;
+},
+setTwo: function(){
+  this.styles.hidden = true;
+},
+  toggle: function () {
+    this.visible = !this.visible;
+  }
 }
 }
 </script>
 
 <template>
-<p :class="selected">Oleg</p>
-<p :class="obj">Tinkoff</p>
+<button class="button" @click="toggle">toggle</button>
+<p class="hunter" v-if="visible">Oleg</p>
+<p :class="styles">Tinkoff</p>
+<button class="button" @click="setDone">visible</button>
+<button class="button" @click="setTwo">hide</button>
 </template>
 
 <style scoped>
-p{color:red}
+p.hidden{
+  display: none;
+}
+p{color:red};
 </style>
