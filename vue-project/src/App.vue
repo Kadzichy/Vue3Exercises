d35f8d8414f2c6c636e1f3a093d002e2db4e819e
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-18 15:44:23 +0300

    Task 40

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,37 +2,18 @@
 export default{
 data(){
   return{
-    visible: true,
-    styles: {
-      hidden: true,
-    },
+    
   }
 },
 methods: {
-setDone:function(){
-  this.styles.hidden = false;
-},
-setTwo: function(){
-  this.styles.hidden = true;
-},
-  toggle: function () {
-    this.visible = !this.visible;
-  }
 }
 }
 </script>
 
 <template>
-<button class="button" @click="toggle">toggle</button>
-<p class="hunter" v-if="visible">Oleg</p>
-<p :class="styles">Tinkoff</p>
-<button class="button" @click="setDone">visible</button>
-<button class="button" @click="setTwo">hide</button>
+<p :class="{active: true, error: false}">Oleg</p>
 </template>
 
 <style scoped>
-p.hidden{
-  display: none;
-}
 p{color:red};
 </style>
