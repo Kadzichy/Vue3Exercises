280d218827aeebcf4d097ced53c3c884e8ac2203
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-18 16:43:21 +0300

    Task 49

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,41 +2,21 @@
 export default{
 data(){
   return{
-    order: '',
-    visible: false,
-    visible1: false,
-    visible2: false,
   }
 },
 methods: {
-one(){
-      this.visible = true;
-},
-  two() {
-    this.visible1 = true;
-  },
-  three() {
-    this.visible2 = true;
-  }
 }
 }
 </script>
 
 <template>
-<div class="one">
-<p>Хто ты?</p>
-<input name="radio" type="radio" v-model="order" value="Хто я" @click="one">
-<label for="">?</label>
-<br>
-<input name="radio" type="radio" v-model="order" value="Розбiйнiк" @click="two">
-<label for="">?</label>
-<br>
-<input name="radio" type="radio" v-model="order" value="Выйди розбiйнiк" @click="three">
-<label for="">?</label>
-</div>
-
-<p> Ваш выбор: {{ order }}</p>
-<br>
+  <p class="one">1+1=?</p>
+    <select class="one" v-model="number">
+      <option>2</option>
+      <option>-0</option>
+      <option>512</option>
+    </select>
+    <p>{{ number }}</p>
 </template>
 
 <style scoped>
