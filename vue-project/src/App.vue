9c7da2e23fd2d96e049cee0f81234a5e4d83aa58
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-18 16:23:09 +0300

    Task 45

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,33 +2,23 @@
 export default{
 data(){
   return{
-    res: 0,
-    sum: 0,
-    num: 0,
-    num1: 0, 
-    num2: 0,
+    two: ' ',
+    // Oleg: ' ',
   }
 },
 methods: {
-  calc: function () {
-    this.res = Math.sqrt(this.num);
-  },
-  summary:function(){
-    this.sum = this.num1 + this.num2;
+list: function(){
   }
 }
 }
 </script>
 
 <template>
-<p class="one">{{ res }}</p>
-<input class="text-field__input" v-model="num">
-<button class="button" @click="calc">Sqrt</button>
-<p class="one">{{ sum }}</p>
-<input class="text-field__input" v-model=num1>
-<input class="text-field__input" v-model=num2>
-<button class="button" @click="summary">Sum</button>
-
+<textarea class="text-field__input" v-model="two"></textarea>
+<p class="one">{{ two }}</p>
+<ul>
+  <li v-for="three in two">{{ three }}</li>
+</ul>
 </template>
 
 <style scoped>
