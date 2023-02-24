500cbf2dadd581e2a1360bc86b3035ed1e7d5364
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-24 18:34:37 +0300

    Task 68

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -3,22 +3,44 @@ import User from './components/Employee.vue'
 export default {
   data() {
     return {
-      
+      Users: [
+        {
+          id: 1,
+          name: 'Oleg',
+          surn: 'Tinkoff'
+        },
+        {
+          id: 2,
+          name: 'Oleg',
+          surn: 'Tinkoff'
+        },
+        {
+          id: 3,
+          name: 'Oleg',
+          surn: 'Tinkoff'
+        },
+      ],
     }
   },
   components: {
     User
   },
   methods: {
-    UserName(name) {
-      console.log(name);
-    },
+    remove(id) {
+      this.Users = this.Users.filter((User) => {
+        return User.id !== id;
+      })
+    }
   }
 }
 </script>
-
 <template>
-<User @show="UserName"/>
+<User v-for   ="User in Users"
+		:id     ="User.id"
+		:name   ="User.name"
+		:surn   ="User.surn"
+		@remove ="remove"
+		:key    ="User.id"/>
 </template>
 
 <style scoped>
