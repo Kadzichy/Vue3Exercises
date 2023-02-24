cab724929ba0ab96b32d7e97f5680976034694e7
Dmitry_Danilov <113195222+Kadzichy@users.noreply.github.com>
2023-02-24 18:13:26 +0300

    Task 62

--- vue-project/src/components/Employee.vue
+++ vue-project/src/components/Employee.vue
@@ -1,33 +1,17 @@
 <script>
-import User from './components/Employee.vue'
 export default {
-  data() {
-    return {
-      users: [
-        {
-          id: 1,
-          name: 'Oleg',
-          salary: 1000,
-          age: 18
-        },
-        {
-          id: 2,
-          name: 'Oleg',
-          salary: 2000,
-          age: 15
-        },
-      ],
+    props: ['name', 'surn', 'salary'],
+    data() {
+        return {
+        }
     }
-  },
-  components: {
-    User
-  }
 }
 </script>
 
 <template>
-<User v-for="User in users" :name="User.name" :salary="User.salary" :age="User.age" :key="User.id" />
+<p class="one">
+{{ name }} 
+{{ surn }}
+{{ salary }} 
+</p>
 </template>
-
-<style scoped>
-</style>
